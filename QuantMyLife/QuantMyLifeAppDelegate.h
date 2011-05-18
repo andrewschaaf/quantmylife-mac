129de7e2fa8b5d8e97f46525314556a8c70d7177
Andrew Schaaf <andrew@andrewschaaf.com>
2011-05-18 10:43:09 -0400

    submodules: foss-credits, mac-preferences

--- QuantMyLife/QuantMyLifeAppDelegate.h
+++ QuantMyLife/QuantMyLifeAppDelegate.h
@@ -1,14 +1,27 @@
 
 #import <Cocoa/Cocoa.h>
+
+#import "AboutController.h"
+#import "PreferencesController.h"
+
 #import "InputIdleSignal.h"
+
 
 @interface QuantMyLifeAppDelegate : NSObject <NSApplicationDelegate> {
 @private
     NSWindow *window;
     
+    AboutController *aboutController;
+    PreferencesController *prefsController;
+    
     NSMutableArray *signals;
 }
 
 @property (assign) IBOutlet NSWindow *window;
+
+
+- (IBAction)showPreferences:(id)sender;
+- (IBAction)showAbout:(id)sender;
+
 
 @end
--- QuantMyLife/QuantMyLifeAppDelegate.m
+++ QuantMyLife/QuantMyLifeAppDelegate.m
@@ -7,9 +7,21 @@
 
 - (void)applicationDidFinishLaunching:(NSNotification *)aNotification
 {
+    aboutController = [[AboutController alloc] init];
+    prefsController = [[PreferencesController alloc] init];
     
     signals = [NSMutableArray array];
     [signals addObject:[[InputIdleSignal alloc] init]];
+}
+
+- (IBAction)showPreferences:(id)sender
+{
+    [prefsController showPreferences];
+}
+
+- (IBAction)showAbout:(id)sender
+{
+    [aboutController showAbout];
 }
 
 @end
